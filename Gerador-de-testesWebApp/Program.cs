d42d9f5a666990454408e27c7db0f809d11fbb69
Vicente Junior <vicente_pc@outlook.com>
2025-07-16 17:26:42 -0300

    Implementado Pacotes PostgreSql, Serilog, Tools

--- Gerador-de-testesWebApp/Program.cs
+++ Gerador-de-testesWebApp/Program.cs
@@ -5,9 +5,22 @@ namespace Gerador_de_testesWebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //builder.Services.AddScoped<IRepositorio, RepositorioEmOrm>()
+
+            builder.Services.AddEntityFrameworkConfig(builder.Configuration);
+            builder.Services.AddSerilogConfig(builder.Logging);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.ApplyMigrations();
+
+            app.UseAntiforgery();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
