2cb37ae92fef1d75b4283906a82c0106fc867167
Iagodizlla <iagoschlemper@gmail.com>
2025-07-17 12:54:40 -0300

    Implementado Arquivo de Usuario Secreto

---
+++ Gerador-de-testesWebApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eAgenda.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("erro")]
+    public IActionResult Erro()
+    {
+        return View();
+    }
+}
