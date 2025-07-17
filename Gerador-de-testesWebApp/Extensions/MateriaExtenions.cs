f3ad9392565e44465efc6aff5a681147c642ce1d
Iagodizlla <iagoschlemper@gmail.com>
2025-07-17 18:28:46 -0300

    Implementado Materia em WebApp

--- Gerador-de-testesWebApp/Extensions/MateriaExtenions.cs
+++ Gerador-de-testesWebApp/Extensions/MateriaExtenions.cs
@@ -1,6 +1,36 @@
-﻿namespace Gerador_de_testesWebApp.Extensions
+﻿using eAgenda.WebApp.Models;
+using Gerador_de_testes.ModuloDisciplina;
+using Gerador_de_testes.ModuloMateria;
+
+namespace eAgenda.WebApp.Extensions;
+
+public static class DespesaExtensions
 {
-    public class MateriaExtenions
+    public static Materia ParaEntidade(this FormularioMateriaViewModel formularioVM, List<Disciplina> disciplinas)
     {
+        Disciplina Selecionado = new Disciplina();
+        foreach (var d in disciplinas)
+        {
+            if (d.Id.Equals(formularioVM.DisciplinaSelecionada))
+            {
+                Selecionado = d;
+                break;
+            }
+        }
+        return new Materia(
+            formularioVM.Nome,
+            formularioVM.Serie,
+            Selecionado
+        );
+    }
+
+    public static DetalhesMateriaViewModel ParaDetalhesVM(this Materia materia)
+    {
+        return new DetalhesMateriaViewModel(
+                materia.Id,
+                materia.Nome,
+                materia.Serie,
+                materia.Disciplina
+        );
     }
 }
