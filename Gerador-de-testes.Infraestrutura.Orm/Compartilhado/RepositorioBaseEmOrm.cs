ae8b7bf4f8294930b2c2260799837e803737b495
Iagodizlla <iagoschlemper@gmail.com>
2025-08-01 14:36:46 -0300

    Implementada classe de configuração de testes TextFixture.cs

--- Gerador-de-testes.Infraestrutura.Orm/Compartilhado/RepositorioBaseEmOrm.cs
+++ Gerador-de-testes.Infraestrutura.Orm/Compartilhado/RepositorioBaseEmOrm.cs
@@ -16,6 +16,10 @@ public class RepositorioBaseEmOrm<T> where T : EntidadeBase<T>
     {
         registros.Add(novoRegistro);
     }
+    public void CadastrarEntidades(IList<T> entidades)
+    {
+        registros.AddRange(entidades);
+    }
 
     public bool Editar(Guid idRegistro, T registroEditado)
     {
